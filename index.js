a83c81436b802856e90c7e9a8104e49e6ce55627
sanjaynathan2006 <sanjaynathan2006@gmail.com>
2026-06-07 21:10:52 +0530

    Add keep-alive server to index.js

--- index.js
+++ index.js
@@ -584,3 +584,12 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 client.login(process.env.TOKEN);
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is alive!');
+});
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Keep-alive server running');
+});
